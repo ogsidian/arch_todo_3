5f96f79eb5bafeeef762c23e8848bde0367717aa
nnan <zarathustrium@yandex.ru>
2021-01-21 19:55:42 +0300

    addTask, remove, Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,88 @@
-import React from 'react';
-import TodoItem from './components/TodoItem';
+import React from "react";
+import TodoItem from "./components/TodoItem";
 
-const colors = ['grey', 'red', 'blue', 'orange', 'green'];
+const colors = ["grey", "red", "blue", "orange", "green"];
 
 function App() {
+  const inputRef = React.useRef();
+  const [activeColor, setActiveColor] = React.useState("");
   const [tasks, setTasks] = React.useState([
     {
-      id: 1,
-      text: 'Попробовать создать ToDo',
+      text: "Попробовать создать ToDo",
+      color: "green",
     },
     {
-      id: 2,
-      text: 'Сохранить задачи в массив стейта',
+      text: "Сохранить задачи в массив стейта",
+      color: "red",
     },
   ]);
+
+  const removeTask = (index) => {
+    setTasks(
+      tasks.filter((obj, i) => {
+        return index !== i;
+      })
+    );
+  };
+
+  const addTask = (text, color) => {
+    const obj = {
+      text,
+      color,
+    };
+    setTasks([...tasks, obj]);
+  };
+
+  const addColor = (color) => {
+    setActiveColor(color);
+  };
+
+  const editTask = (index) => {
+    const text = tasks[index].text;
+    const newTask = window.prompt("Введите ", text);
+    if (newTask) {
+      setTasks(
+        tasks.map((item, i) => ({ text: index === i ? newTask : item.text }))
+      );
+    }
+  };
+
+  const searchEnter = (event, color) => {
+    if (event.key === "Enter") {
+      addTask(event.target.value, activeColor || "red");
+      event.target.value = "";
+    }
+  };
 
   return (
     <div className="App">
       <div className="todo">
         <h2>Список задач</h2>
-        {tasks.map((obj) => (
-          <TodoItem key={obj.id} text={obj.text} />
+        {tasks.map((obj, index) => (
+          <TodoItem
+            key={obj.id}
+            index={index}
+            text={obj.text}
+            editTask={editTask}
+            removeTask={removeTask}
+            color={obj.color}
+          />
         ))}
         <div className="todo-input">
-          <input type="text" placeholder="Текст задачи..." />
+          <input
+            type="text"
+            placeholder="Текст задачи..."
+            onKeyUp={searchEnter}
+          />
           <ul>
             {colors.map((color) => (
-              <li className={`todo-color ${color}`}></li>
+              <li
+                style={{ backgroundColor: `var(--${color})` }}
+                className={`todo-color ${
+                  activeColor === color ? "active" : ""
+                }`}
+                onClick={() => addColor(color)}
+              ></li>
             ))}
           </ul>
         </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 function TodoItem(props) {
   return (
     <div className="todo-item">
@@ -11,13 +13,24 @@ function TodoItem(props) {
           </div>
         </label>
       </div>
-      <div className="todo-delimiter"></div>
+      <div
+        className="todo-delimiter"
+        style={{ backgroundColor: `var(--${props.color})` }}
+      ></div>
       <p className="todo-text">{props.text}</p>
       <div className="todo-actions">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 384">
+        <svg
+          onClick={() => props.editTask(props.index)}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 384"
+        >
           <path d="M0 304v80h80l236-236-80-80zM378 56L328 6c-8-8-22-8-30 0l-39 39 80 80 39-39c8-8 8-22 0-30z"></path>
         </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
+        <svg
+          onClick={() => props.removeTask(props.index)}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 96 96"
+        >
           <path d="M63 48l32-33v-2L83 1a2 2 0 00-2 0L48 33 15 1h-2L1 13v2l32 33L1 81v2l12 12a2 2 0 002 0l33-32 33 32a2 2 0 002 0l12-12v-2L63 48z"></path>
         </svg>
       </div>
